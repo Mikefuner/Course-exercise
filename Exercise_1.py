da05bd8b420b352fc0ff2de8b656d18309288d27
Mykhailo Onufriiev <onufriev.m@gmail.com>
2024-01-02 14:31:30 +0100

    Added possibility of insert so much lists how you want and messages on the inputs.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,4 +1,10 @@
-sides = eval(input())
-sides.sort()
+while True:
+    sides = eval(input('Insert list --> '))
+    sides.sort()
 
-print(sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2)
+    print(sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2)
+    message = input('Do you want to insert other list? --> ')
+
+    if message.lower() == 'no':
+        break
+print('Thank you. Goodbye!')
